639de31c6398cdcca2f35dea14ab1839efd4a693
abhikamboji <abhi.kamboji@gmail.com>
2017-08-06 00:17:19 -0500

    Implemented Part of 3D Calibration

--- app/src/main/java/com/example/michael/antislouch/menu.java
+++ app/src/main/java/com/example/michael/antislouch/menu.java
@@ -1,6 +1,7 @@
 package com.example.michael.antislouch;
 
 import android.content.Context;
+import android.content.Intent;
 import android.hardware.Sensor;
 import android.hardware.SensorManager;
 import android.os.Bundle;
@@ -11,20 +12,36 @@ import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class menu extends AppCompatActivity {
 
     private SensorManager mSensorManager;
     private Sensor mSensor;
+    Button graph_menu;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_menu);
-
+        buttonListener();
 
 
     }
 
+
+
+    public void buttonListener(){
+        final Context context = this;
+        graph_menu = (Button) findViewById(R.id.Graph);
+        graph_menu.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent graphIntent =  new Intent(context, graph_menu.class);
+                startActivity(graphIntent);
+            }
+        });
+
+    }
 
     //This returns the angle orientation by getting x,y,z
     private int angleOrientation(){
@@ -34,14 +51,12 @@ public class menu extends AppCompatActivity {
 
     }
 
-    @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_menu, menu);
         return true;
     }
 
-    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         // Handle action bar item clicks here. The action bar will
         // automatically handle clicks on the Home/Up button, so long
