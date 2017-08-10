51dd857b3668d11ea1addb2719f0df566744b508
Thads Michael Egar <ThadsMichaelEgar@users.noreply.github.com>
2017-08-09 23:19:23 -0500

    Some settings changes to segue into more complex tasks. Sound tests in settings.

--- app/src/main/java/com/example/michael/antislouch/settings.java
+++ app/src/main/java/com/example/michael/antislouch/settings.java
@@ -1,13 +1,14 @@
 package com.example.michael.antislouch;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.media.MediaPlayer;
+import android.widget.Button;
 
 public class settings extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,14 +17,22 @@ public class settings extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
+        //create media player
+        final MediaPlayer soundEnable = MediaPlayer.create(this, R.raw.chimesup);
+
+        //Enable sound button/Create Sound button
+        Button playSoundButton = (Button)findViewById(R.id.Sound_Enable);
+        playSoundButton.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+            public void onClick(View v) {
+                soundEnable.start();
             }
         });
+
+
+
     }
+
+
 
 }
